469b13d770ac628f91ebffe67d6c9e6d20907a15
Gregor Middell <gregor@middell.net>
2012-11-22 14:59:35 +0100

    Configuration of HTTP server; dependency injection; H2 Text repository singleton

--- src/main/java/eu/interedition/text/http/LayerResource.java
+++ src/main/java/eu/interedition/text/http/LayerResource.java
@@ -1,10 +1,22 @@
 package eu.interedition.text.http;
 
+import com.google.inject.Inject;
+import eu.interedition.text.h2.H2TextRepository;
 import javax.ws.rs.Path;
+import org.codehaus.jackson.JsonNode;
 
 /**
  * @author <a href="http://gregor.middell.net/" title="Homepage">Gregor Middell</a>
  */
 @Path("/")
 public class LayerResource {
+
+    private final H2TextRepository<JsonNode> textRepository;
+
+    @Inject
+    public LayerResource(H2TextRepository<JsonNode> textRepository) {
+        this.textRepository = textRepository;
+    }
+
+
 }
